680c53200253a9d9840b65ed7e08ea119b0e026e
TristanSmithlib <tchambers@smith.edu>
2019-01-29 11:01:32 -0500

    fix tests

--- batch2imi/tests/run-tests.sh
+++ batch2imi/tests/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python3 ../batch2imi.py sample-batch/ test-output.csv smith:test --remote-path /ingest/dir/ > test-output.log 2>&1
-md5 test-output.csv
-md5 correct-output/test-output.csv
-md5 test-output.log
-md5 correct-output/test-output.log
+python3 ../batch2imi.py sample-batch/ test-output smith:test --remote-path /ingest/dir/ > test-output.log 2>&1
+md5sum test-output.csv
+md5sum correct-output/test-output.csv
+md5sum test-output.log
+md5sum correct-output/test-output.log
